5984c141d473fbaf3950e679bb2e0e0de10938de
kazumasamatsumoto <kazumasa.matsumoto62@gmail.com>
2022-05-05 14:22:56 +0900

    address hyouzi

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 const symbol = require('/node_modules/symbol-sdk')
 
-console.log("Hello Symbol")
+const address = symbol.Address.createFromRawAddress("TDIR3I5NU2HPZ7KTNMMB4YWSIPP2X4LWVFOCIWY")
+console.log(`Your address : ${address.plain()}`)
