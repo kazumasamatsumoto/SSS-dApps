29a4861d631345ea68598713ea01c378eda8e63d
kazumasamatsumoto <kazumasa.matsumoto62@gmail.com>
2022-05-05 15:28:18 +0900

    動作確認

--- script.js
+++ script.js
@@ -17,62 +17,63 @@ dom_addr.innerText = address.pretty();
 
 const repositoryFactory = new symbol.RepositoryFactoryHttp(NODE_URL);
 const accountHttp = repositoryFactory.createAccountRepository();
+const transactionHttp = repositoryFactory.createTransactionRepository();
 
-accountHttp
-  .getAccountInfo(address)
-  .toPromise()
-  .then((accountInfo) => {
-    for (let m of accountInfo.mosaics) {
-      if (m.id.id.toHex() === XYM_ID) {
-        const dom_xym = document.getElementById("wallet-xym");
-        dom_xym.innerText = `XYM Balance : ${
-          m.amount.compact() / Math.pow(10, 6)
-        }`;
+setTimeout(() => {
+  accountHttp
+    .getAccountInfo(address)
+    .toPromise()
+    .then((accountInfo) => {
+      for (let m of accountInfo.mosaics) {
+        if (m.id.id.toHex() === XYM_ID) {
+          const dom_xym = document.getElementById("wallet-xym");
+          dom_xym.innerText = `XYM Balance : ${
+            m.amount.compact() / Math.pow(10, 6)
+          }`;
+        }
       }
-    }
-  });
+    });
+  const searchCriteria = {
+    group: symbol.TransactionGroup.Confirmed,
+    address,
+    pageNumber: 1,
+    pageSize: 20,
+    order: symbol.Order.Desc,
+  };
+  transactionHttp
+    .search(searchCriteria)
+    .toPromise()
+    .then((txs) => {
+      const dom_txInfo = document.getElementById("wallet-transactions");
+      for (let tx of txs.data) {
+        const dom_tx = document.createElement("div");
+        const dom_txType = document.createElement("div");
+        const dom_hash = document.createElement("div");
 
+        dom_txType.innerText = `Transaction Type : ${getTransactionType(
+          tx.type
+        )}`;
+        dom_hash.innerText = `Transaction Hash : ${tx.transactionInfo.hash}`;
 
-const transactionHttp = repositoryFactory.createTransactionRepository()
-const searchCriteria = {
-  group: symbol.TransactionGroup.Confirmed,
-  address,
-  pageNumber: 1,
-  pageSize: 20,
-  order: symbol.Order.Desc,
+        dom_tx.appendChild(dom_txType);
+        dom_tx.appendChild(dom_hash);
+        dom_tx.appendChild(document.createElement("hr"));
+
+        dom_txInfo.appendChild(dom_tx);
+      }
+    });
+}, 500);
+
+function getTransactionType(type) {
+  if (type === 16724) return "TRANSFER TRANSACTION";
+  return "OTHER TRANSACTION";
 }
 
-transactionHttp
-  .search(searchCriteria)
-  .toPromise()
-  .then((txs) => {
-    const dom_txInfo = document.getElementById('wallet-transactions')
-    for (let tx of txs.data) {
-      const dom_tx = document.createElement('div')
-      const dom_txType = document.createElement('div')
-      const dom_hash = document.createElement('div')
-
-      dom_txType.innerText = `Transaction Type : ${getTransactionType(tx.type)}`
-      dom_hash.innerText = `Transaction Hash : ${tx.transactionInfo.hash}`
-
-      dom_tx.appendChild(dom_txType)
-      dom_tx.appendChild(dom_hash)
-      dom_tx.appendChild(document.createElement('hr'))
-
-      dom_txInfo.appendChild(dom_tx)
-    }
-  })
-
-  function getTransactionType (type) {
-    if (type === 16724) return 'TRANSFER TRANSACTION'
-    return 'OTHER TRANSACTION'
-  }
-
 function handleClick() {
-  const addr = document.getElementById('form-addr').value
-  const amount = document.getElementById('form-amount').value
-  const message = document.getElementById('form-message').value
-  const pk = document.getElementById('form-pk').value
+  const addr = document.getElementById("form-addr").value;
+  const amount = document.getElementById("form-amount").value;
+  const message = document.getElementById("form-message").value;
+  const pk = document.getElementById("form-pk").value;
 
   const tx = symbol.TransferTransaction.create(
     symbol.Deadline.create(EPOCH),
@@ -81,16 +82,42 @@ function handleClick() {
       new symbol.Mosaic(
         new symbol.MosaicId(XYM_ID),
         symbol.UInt64.fromUint(Number(amount) * Math.pow(10, 6))
-      )
+      ),
     ],
     symbol.PlainMessage.create(message),
     NET_TYPE,
     symbol.UInt64.fromUint(2000000)
-  )
+  );
 
-  const acc = symbol.Account.createFromPrivateKey(pk, NET_TYPE)
+  const acc = symbol.Account.createFromPrivateKey(pk, NET_TYPE);
 
-  const signedTx = acc.sign(tx, GENERATION_HASH)
+  const signedTx = acc.sign(tx, GENERATION_HASH);
 
-  transactionHttp.announce(signedTx)
+  transactionHttp.announce(signedTx);
+}
+
+function handleSSS() {
+  const addr = document.getElementById("form-addr").value;
+  const amount = document.getElementById("form-amount").value;
+  const message = document.getElementById("form-message").value;
+  const tx = symbol.TransferTransaction.create(
+    symbol.Deadline.create(EPOCH),
+    symbol.Address.createFromRawAddress(addr),
+    [
+      new symbol.Mosaic(
+        new symbol.MosaicId(XYM_ID),
+        symbol.UInt64.fromUint(Number(amount) * Math.pow(10, 6))
+      ),
+    ],
+    symbol.PlainMessage.create(message),
+    NET_TYPE,
+    symbol.UInt64.fromUint(2000000)
+  );
+
+  window.SSS.setTransaction(tx);
+
+  window.SSS.requestSign().then((signedTx) => {
+    console.log("signedTx", signedTx);
+    transactionHttp.announce(signedTx);
+  });
 }
