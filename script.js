a21d0de5a80176b03848019802a1274f1e4a485f
kazumasamatsumoto <kazumasa.matsumoto62@gmail.com>
2022-05-05 14:27:43 +0900

    アドレスの表示

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ const symbol = require('/node_modules/symbol-sdk')
 
 const address = symbol.Address.createFromRawAddress("TDIR3I5NU2HPZ7KTNMMB4YWSIPP2X4LWVFOCIWY")
 console.log(`Your address : ${address.plain()}`)
+
+const dom_addr = document.getElementById('wallet-addr')
+dom_addr.innerText = address.pretty()
